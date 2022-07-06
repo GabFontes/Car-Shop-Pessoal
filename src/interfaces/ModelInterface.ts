771f76155f00221dc41797004056648918a63ea6
gab <gabrielfontes321@gmail.com>
2022-07-06 19:06:04 -0300

    feat: fixing wrong export

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,10 +1,7 @@
-interface GenericInterface<T> {
+export interface Model<T> {
   create(Object: T): Promise<T>
   read(): Promise<T[]>
   readOne(string: string): Promise<T | null>
   update(string: string, Object: T): Promise<T | null>
   delete(string: string): Promise<T | null>
 }
-
-// eslint-disable-next-line import/prefer-default-export
-export { GenericInterface as Model };
