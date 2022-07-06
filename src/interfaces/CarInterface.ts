5e5d4bf7a544f0c8ea5ba565e3e2110fb2cb77ee
gab <gabrielfontes321@gmail.com>
2022-07-06 20:48:47 -0300

    feat: implementing Generic Car Interface

---
+++ src/interfaces/CarInterface.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
+
+const CarSchema = z.object({
+  doorsQty: z.number({
+    required_error: 'doorsQty is required',
+    invalid_type_error: 'doorsQty must be a number',
+  }).gte(2).lte(4),
+  seatsQty: z.number({
+    required_error: 'seatsQty is required',
+    invalid_type_error: 'seatsQty must be a number',
+  }).gte(2).lte(7),
+});
+
+const CarVehicle = z.intersection(CarSchema, VehicleSchema);
+
+export type Car = z.infer<typeof CarVehicle>;
