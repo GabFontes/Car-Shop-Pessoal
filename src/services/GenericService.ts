fcee86a1f5900fb7e1f86f078a8b33986ff867ed
gab <gabrielfontes321@gmail.com>
2022-07-08 17:20:01 -0300

    feat(src/services): implementing GenericService

---
+++ src/services/GenericService.ts
@@ -0,0 +1,22 @@
+// src/services/index.ts
+
+import { ZodError } from 'zod';
+import { Model } from '../interfaces/ModelInterface';
+
+export interface ServiceError {
+  error: ZodError;
+}
+
+export default abstract class GenericService<T> {
+  constructor(protected model: Model<T>) { }
+
+  abstract read(): Promise<T[]>;
+
+  abstract readOne(id: string): Promise<T | null>;
+
+  abstract create(obj: T): Promise<T | null | ServiceError>;
+
+  abstract update(id: string, obj: T): Promise<T | null | ServiceError>;
+
+  abstract delete(id: string): Promise<T | null>;
+}
