bdb1f112b4239cbf4da63955558b3a3ccc0cc554
gab <gabrielfontes321@gmail.com>
2022-07-08 17:20:47 -0300

    feat(src/controllers): implementing GenericController

---
+++ src/controllers/GenericController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import GenericService from '../services/GenericService';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  minIdLength = 'Id must have 24 hexadecimal characters',
+  badRequest = 'Bad request',
+}
+
+export default abstract class GenericController<T> {
+  abstract route: string;
+
+  protected errors;
+
+  constructor(protected service: GenericService<T>) {
+    this.errors = ControllerErrors;
+  }
+
+  abstract create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  abstract read(
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res>;
+
+  abstract readOne(
+    req: Request<{ id: string; }>,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
+
+  abstract update(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
+
+  abstract delete(
+    req: Request<{ id: string; }>,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
+}
