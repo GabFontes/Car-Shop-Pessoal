1d51bcd91cbc31e0945c600f78676cb538ea1831
gab <gabrielfontes321@gmail.com>
2022-07-08 17:21:35 -0300

    feat(src/routes): implementing CustomRouter

---
+++ src/routes/Router.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import GenericController from '../controllers/GenericController';
+
+export default class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(
+    controller: GenericController<T>,
+    route: string = controller.route,
+  ) {
+    this.router.get(route, controller.read);
+    this.router.get(`${route}/:id`, controller.readOne);
+    this.router.post(route, controller.create);
+    this.router.delete(`${route}/:id`, controller.delete);
+    this.router.put(`${route}/:id`, controller.update);
+  }
+}
